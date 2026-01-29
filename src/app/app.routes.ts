b4e9c32a7147190e375704c9f82a7cddde160dff
Juan Pedro Adamuz Canales <juanpework@gmail.com>
2026-01-29 18:25:01 +0100

    feat: core configuration and setup
    - Configure Tailwind CSS and DaisyUI
    - Add Spanish locale for dates
    - Setup routing structure
    - Configure TypeScript paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,78 @@
 import { Routes } from '@angular/router';
+import { financeInitGuard } from '@core/guards/finance-init.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'finance-space',
+    pathMatch: 'full',
+  },
+  {
+    path: 'welcome',
+    loadComponent: () =>
+      import('./pages/welcome/welcome'),
+  },
+  {
+    path: 'setup',
+    loadComponent: () =>
+      import('./pages/setup/setup'),
+  },
+  {
+    path: 'setup/:id',
+    loadComponent: () =>
+      import('./pages/setup/setup'),
+  },
+  {
+    path: 'finance-space',
+    canActivate: [financeInitGuard],
+    loadComponent: () => import('./layout/finance-space/finance-space'),
+    children: [
+      {
+        path: '',
+        redirectTo: 'movements',
+        pathMatch: 'full',
+      },
+      {
+        path: 'movements',
+        children: [
+          {
+            path: '',
+            loadComponent: () => import('./pages/movements/movements'),
+          },
+          {
+            path: 'new',
+            loadComponent: () => import('./pages/movement-form/movement-form')
+          },
+          {
+            path: ':id',
+            loadComponent: () => import('./pages/movement-detail/movement-detail')
+          },
+          {
+            path: ':id/edit',
+            loadComponent: () => import('./pages/movement-form/movement-form')
+          }
+        ]
+      },
+      {
+        path: 'summary',
+        loadComponent: () => import('./pages/summary/summary')
+      },
+      {
+        path: 'salary-list',
+        loadComponent: () => import('./pages/salary-list/salary-list')
+      },
+      {
+        path: 'saving',
+        loadComponent: () => import('./pages/saving/saving')
+      },
+      {
+        path: '**',
+        redirectTo: 'home'
+      },
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'welcome'
+  }
+];
