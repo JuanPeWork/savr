7abdf9d168e599e71eb490d4eb5d52028d678ec0
Juan Pedro Adamuz Canales <juanpework@gmail.com>
2026-02-07 14:45:18 +0100

    chore: add Firebase deps and simplify app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,18 @@
-import { ApplicationConfig, InjectionToken, LOCALE_ID, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
-import { StoragePort } from '@core/storage/storage-port.interface';
-import { LocalForageStorageService } from '@core/storage/localforage-storage.service';
+import { environment } from '../environments/environment';
+import { SALARY_COLLECTION, MOVEMENT_COLLECTION, provideFirestoreCollection } from '@core/storage/collection.tokens';
 
 registerLocaleData(localeEs, 'es');
-
-export const STORAGE = new InjectionToken<StoragePort>('FINANCE_STORAGE');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,10 +23,11 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
     { provide: LOCALE_ID, useValue: 'es' },
-    {
-      provide: STORAGE,
-      useClass: LocalForageStorageService,
-    }
+    provideFirestoreCollection(SALARY_COLLECTION, 'salaries'),
+    provideFirestoreCollection(MOVEMENT_COLLECTION, 'movements')
   ]
 };
